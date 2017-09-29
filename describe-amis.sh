e364d39eb91e616b81ec9dda5360ce1c37ce5b62
Nag Medida <nmedida@netflix.com>
2017-09-28 19:45:13 -0700

    Script to get all the AMI's from all the regions

---
+++ describe-amis.sh
@@ -0,0 +1,15 @@
+aws ec2 describe-images --executable-users all --region ap-south-1 > ap-south-1_ami.json
+aws ec2 describe-images --executable-users all --region eu-west-2 > eu-west-2_ami.json
+aws ec2 describe-images --executable-users all --region eu-west-1 > eu-west-1_ami.json
+aws ec2 describe-images --executable-users all --region ap-northeast-2 > ap-northeast-2_ami.json
+aws ec2 describe-images --executable-users all --region ap-northeast-1 > ap-northeast-1_ami.json
+aws ec2 describe-images --executable-users all --region sa-east-1 > sa-east-1_ami.json
+aws ec2 describe-images --executable-users all --region ca-central-1 > ca-central-1_ami.json
+aws ec2 describe-images --executable-users all --region ap-southeast-1 > ap-southeast-1_ami.json
+aws ec2 describe-images --executable-users all --region ap-southeast-2 > ap-southeast-2_ami.json
+aws ec2 describe-images --executable-users all --region eu-central-1 > eu-central-1_ami.json
+aws ec2 describe-images --executable-users all --region us-east-1 > us-east-1_ami.json
+aws ec2 describe-images --executable-users all --region us-east-2 > us-east-2_ami.json
+aws ec2 describe-images --executable-users all --region us-west-1 > us-west-1_ami.json
+aws ec2 describe-images --executable-users all --region us-west-2 > us-west-2_ami.json
+
